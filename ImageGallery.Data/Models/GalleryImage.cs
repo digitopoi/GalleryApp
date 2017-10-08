98fe92f96d8050945e0f017e142d49f4e01d65ad
digitopoi <hubo85@gmail.com>
2017-10-08 13:29:20 -0500

    Modify Tags field to be a collection of Tag

--- ImageGallery.Data/Models/GalleryImage.cs
+++ ImageGallery.Data/Models/GalleryImage.cs
@@ -9,6 +9,6 @@ namespace ImageGallery.Data.Models
         public string Title { get; set; }
         public DateTime Created { get; set; }
         public string Url { get; set; }
-        public IEnumerable<string> Tags { get; set; }
+        public virtual IEnumerable<Tag> Tags { get; set; }
     }
 }
