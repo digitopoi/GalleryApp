9cbf29bce8c35650825b2e1ccf735e3244990bdf
digitopoi <hubo85@gmail.com>
2017-10-08 13:08:44 -0500

    Add Id field primary key to gallery image class

--- ImageGallery.Data/Models/GalleryImage.cs
+++ ImageGallery.Data/Models/GalleryImage.cs
@@ -6,6 +6,7 @@ namespace ImageGallery.Data.Models
 {
     public class GalleryImage
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public DateTime Created { get; set; }
         public string Url { get; set; }
