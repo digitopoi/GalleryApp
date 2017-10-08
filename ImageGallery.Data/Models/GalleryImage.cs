76b00da3a16602b9e44a6919991dca7e22839222
digitopoi <hubo85@gmail.com>
2017-10-08 13:07:30 -0500

    Add gallery image domain model

---
+++ ImageGallery.Data/Models/GalleryImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageGallery.Data.Models
+{
+    public class GalleryImage
+    {
+        public string Title { get; set; }
+        public DateTime Created { get; set; }
+        public string Url { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+    }
+}
