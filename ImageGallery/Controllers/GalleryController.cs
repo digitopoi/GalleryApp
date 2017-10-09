be4c4965e9ae604015b5557461fa01c5b246d408
digitopoi <hubo85@gmail.com>
2017-10-09 13:23:28 -0500

    Add mock data

--- ImageGallery/Controllers/GalleryController.cs
+++ ImageGallery/Controllers/GalleryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ImageGallery.Models;
+using System.Collections.Generic;
 
 namespace ImageGallery.Controllers
 {
@@ -7,9 +8,55 @@ namespace ImageGallery.Controllers
     {
         public IActionResult Index()
         {
+            var hikingImageTags = new List<ImageTag>();
+            var cityImageTags = new List<ImageTag>();
+
+            var tag1 = new ImageTag()
+            {
+                Description = "Adventure",
+                Id = 0
+            };
+
+            var tag2 = new ImageTag()
+            {
+                Description = "Urban",
+                Id = 1
+            };
+
+            var tag3 = new ImageTag()
+            {
+                Description = "New York",
+                Id = 2
+            };
+
+            var imageList = new List<GalleryImage>()
+            {
+                new GalleryImage()
+                {
+                    Title = "Hiking Trip",
+                    Url = "",
+                    Created = System.DateTime.Now,
+                    Tags = hikingImageTags
+                },
+                new GalleryImage()
+                {
+                    Title = "On the Trail",
+                    Url = "",
+                    Created = System.DateTime.Now,
+                    Tags = hikingImageTags
+                },
+                new GalleryImage()
+                {
+                    Title = "Downtown",
+                    Url = "",
+                    Created = System.DateTime.Now,
+                    Tags = cityImageTags
+                }
+            };
+
             var model = new GalleryIndexModel()
             {
-
+                Images = imageList
             };
 
             return View(model);
